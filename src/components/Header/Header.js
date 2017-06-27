ba246eb9ba9c9c352c5e72165d0e63110bc3ce5f
Keshal Desai <novalearngood@gmail.com>
2017-06-27 13:38:51 -0400

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import logo from "../../images/logo.svg";
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        Logo
-        Browse
-        Search on right
+        <div className="header-left">
+          <img src={logo} height="30" />
+          Browse
+          Kids
+          DVD
+        </div>
+        <div className="header-right">
+          Search on right
+          Notifs on right
+          Userprofile on right
+        </div>
       </div>
     );
   }
