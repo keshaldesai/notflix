58aeb360c1ca6b3c7cd0e41d7b9b9b309d147b07
Keshal Desai <novalearngood@gmail.com>
2017-06-27 13:30:08 -0400

    header setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        App
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        Logo
+        Browse
+        Search on right
+      </div>
+    );
+  }
+}
+
+export default Header;
