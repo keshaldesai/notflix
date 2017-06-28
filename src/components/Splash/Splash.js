d8e1155e11becbb84ac74ec86f7e1fc5d3cd5371
Keshal Desai <novalearngood@gmail.com>
2017-06-28 14:43:40 -0400

    splash grid

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -5,7 +5,11 @@ import splashTitle from "../../images/splashtitle.webp";
 const Splash = () => {
   return (
     <div className="splash" style={{ backgroundImage: `url(${splash})` }}>
-      <img src={splashTitle} alt="splash-title" />
+      <img src={splashTitle} alt="splash-title" className="splash-title" />
+      <div />
+      He's a half-horse, half-man, has-been TV star who drinks a bit too much.
+      He's really got a lot going on right now.
+      Play My List
     </div>
   );
 };
