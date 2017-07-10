21bbbf1b2082cde9827ac65769217d7434810a89
Keshal Desai <novalearngood@gmail.com>
2017-07-10 12:32:20 -0400

    working movie overlay

--- src/components/Movies/SingleMovie.js
+++ src/components/Movies/SingleMovie.js
@@ -1,29 +1,18 @@
 import React, { Component } from "react";
 
 class SingleMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showPreview: false
-    };
-  }
 
   render() {
     const { poster_path } = this.props.movie;
-    const { showPreview } = this.state;
     const imgSrc = `https://image.tmdb.org/t/p/w342${poster_path}`;
-    const previewClass = showPreview ? " movie-preview" : "";
     return (
-      <div
-        className={"movie-single" + previewClass}
-        onMouseOver={() => {
-          this.setState({ showPreview: true });
-        }}
-        onMouseLeave={() => {
-          this.setState({ showPreview: false });
-        }}
-      >
+      <div className="movie-single" >
         <img src={imgSrc} alt="movie poster" />
+        <div className="movie-overlay">
+          <span>
+            Overlay
+          </span>
+        </div>
       </div>
     );
   }
