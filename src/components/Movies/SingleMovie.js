bbfbaad77e01ecf7e4369d3b8ac66fa978f717e9
Keshal Desai <novalearngood@gmail.com>
2017-07-10 13:05:05 -0400

    overlay info setup

--- src/components/Movies/SingleMovie.js
+++ src/components/Movies/SingleMovie.js
@@ -9,9 +9,22 @@ class SingleMovie extends Component {
       <div className="movie-single" >
         <img src={imgSrc} alt="movie poster" />
         <div className="movie-overlay">
-          <span>
-            Overlay
-          </span>
+          <div className="overlay-grid">
+            <div className="overlay-info">
+              <div>
+                <div>Moana</div>
+                <div>98% Match 2016 PG 1h 47m</div>
+                <div>A Polynesian girl destined to be her island's chief makes a perilous trip with a boastful demigod to undo his mistake and save her people.</div>
+              </div>
+            </div>
+            <div className="overlay-prefs">
+              <div>
+                <div>UP</div>
+                <div>DO</div>
+                <div>ADD</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
